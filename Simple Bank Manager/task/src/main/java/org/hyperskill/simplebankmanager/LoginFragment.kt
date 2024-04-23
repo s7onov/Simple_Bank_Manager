7dd094e1e1c924ff5231bf10cd9b809691c465d6
Sergey Semenov <sergey7onov@gmail.com>
2024-04-23 22:08:54 +0600

    Initial commit

---
+++ Simple Bank Manager/task/src/main/java/org/hyperskill/simplebankmanager/LoginFragment.kt
@@ -0,0 +1,83 @@
+package org.hyperskill.simplebankmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "username"
+private const val ARG_PARAM2 = "password"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var correctUsername: String
+    private lateinit var correctPassword: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val intent = (view.context as MainActivity).intent
+        correctUsername = intent.extras?.getString("username") ?: "Lara"
+        correctPassword = intent.extras?.getString("password") ?: "1234"
+        val button = view.findViewById<Button>(R.id.loginButton)
+        button.setOnClickListener {
+            val enteredName = view.findViewById<EditText>(R.id.loginUsername).text.toString()
+            val enteredPass = view.findViewById<EditText>(R.id.loginPassword).text.toString()
+            if(enteredName == correctUsername && enteredPass == correctPassword) {
+                Toast.makeText(context, "logged in", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_loginFragment_to_userMenuFragment,
+                    bundleOf("username" to enteredName))
+            } else Toast.makeText(context, "invalid credentials", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LoginFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LoginFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
